c5e64602b80225ad5b6e5bf10b35492e00a332ee
compu2 <compu2@hanmail.net>
2023-10-24 18:21:55 +0900

    localdb edit

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -7,17 +7,16 @@ import Navbar from '../component/Navbar';
 
 const ProductAll = () => {
   const [productList, setProductList] = useState([]);
- const [query, setQuery] = useSearchParams();
-  /* const [query] = useSearchParams(); */
+ /* const [query, setQuery] = useSearchParams(); */
+  const [query] = useSearchParams(); 
   const getProducts = async() => {
     //let url = `http://localhost:5000/products/`
 
     let searchQuery = query.get('q')||"";
     console.log(searchQuery)
 
-    /* let url = `http://localhost:5000/products?q=${searchQuery}` 데이터확인O     
-    /* let url = `https://my-json-server.typicode.com/learn2306/meal/main/db.json/products?q=${searchQuery}` */ 
-    let url = `https://my-json-server.typicode.com/learn2306/meal/products?q=${searchQuery}`
+    let url = `http://localhost:5000/products?q=${searchQuery}` //데이터확인O        
+    /* let url = `https://my-json-server.typicode.com/learn2306/meal/products?q=${searchQuery}`*/   
     let response = await fetch(url);
     let data = await response.json();
     console.log(data)
