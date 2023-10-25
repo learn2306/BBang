a5bf1258ff1b252a768830274d81f5e46963a6aa
compu2 <compu2@hanmail.net>
2023-10-25 13:31:35 +0900

    again first

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -7,20 +7,19 @@ import Navbar from '../component/Navbar';
 
 const ProductAll = () => {
   const [productList, setProductList] = useState([]);
- /* const [query, setQuery] = useSearchParams(); */
-  const [query] = useSearchParams(); 
+  const [query, setQuery] = useSearchParams();   
   const getProducts = async() => {
     //let url = `http://localhost:5000/products/`
 
-    let searchQuery = query.get('q')||"";
-    console.log(searchQuery)
+      let searchQuery = query.get('q')||"";
+      console.log(searchQuery)
 
-    let url = `http://localhost:5000/products?q=${searchQuery}` //데이터확인O        
-    /* let url = `https://my-json-server.typicode.com/learn2306/meal/products?q=${searchQuery}`*/   
-    let response = await fetch(url);
-    let data = await response.json();
-    console.log(data)
-    setProductList(data)  
+      /* let url = `http://localhost:5000/products?q=${searchQuery}` */  //데이터확인O        
+      let url = `https://my-json-server.typicode.com/learn2306/meal/products?q=${searchQuery}`
+      let response = await fetch(url);
+      let data = await response.json();
+      console.log(data)
+      setProductList(data)  
     }
 
     useEffect(() => {
